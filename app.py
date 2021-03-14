4d384b68805a5b30e26f21e1125d7a2ad35bd926
tirthpatel98 <tirthpatel199820@gmail.com>
2021-03-13 21:33:55 -0800

    google

---
+++ app.py
@@ -0,0 +1,128 @@
+import streamlit as st
+import pandas as pd
+import json
+import requests
+import plotly.graph_objects as go
+from Linkedin_scraping import get_linkedin_info
+from places_api import google_data
+
+GMAPS_API_KEY="AIzaSyBja88VRg3fkfpYcseDWpQXuVuOGdLKEBI"
+
+def main():
+    st.set_page_config(page_title='ESDC', page_icon=':office:',layout='wide')
+    st.sidebar.title("Menu")
+    app_mode = st.sidebar.selectbox("Please select a page", ["MainPage", "Business Tracker"])
+
+    if app_mode=="MainPage":
+        load_mainpage()
+    
+    elif app_mode=="Business Tracker":
+        load_business()
+    
+
+def load_mainpage():
+    st.header("Welcome To the Business Tracker")
+
+def check_nan(x):
+    if type(x)==float:
+        return [0,0]
+    else:
+        return json.loads(x)['coordinates']
+
+def lat(x):
+        return x[1]
+
+def lon(x):
+    return x[0]
+
+@st.cache
+def load_data():
+    df = pd.read_csv('business-licences-hackathon.csv', sep=';')  
+    df['Geom'] = df['Geom'].apply(check_nan)
+    df['lat'] = df['Geom'].apply(lat)
+    df['lon'] = df['Geom'].apply(lon)
+    return df
+
+def process_issue(df, business_name):
+    df_van = df[df['City']=='Vancouver']
+    df_company = df_van[df_van['BusinessName']==business_name]
+    df_company = df_company[['LicenceNumber','Status','IssuedDate','ExpiredDate']]
+    df_company['Year'] = df_company['LicenceNumber'].apply(lambda x: "20"+str(x[:2]))
+    df_company['color'] = df['Status'].apply(lambda status:color_code(status))
+    df_company.loc[df_company.IssuedDate.isnull(), 'IssuedDate'] = 'N/A'
+    df_company.loc[df_company.ExpiredDate.isnull(), 'ExpiredDate'] = 'N/A'
+    df_company = df_company.sort_values(by=['Year'])
+    return df_company
+
+def render_plot(df_company):
+    fig = go.Figure(data=[go.Table(header=dict(values=["<b>Year<b>", "<b>Status</b>","<b>IssuedDate<b>","<b>ExpiredDate<b>"],line_color='white', fill_color='lightskyblue',align='center', font=dict(color='black', size=12)),cells=dict(
+    values=[df_company.Year, df_company.Status, df_company.IssuedDate, df_company.ExpiredDate],
+    line_color=[df_company.color], fill_color=[df_company.color],
+    align='center', font=dict(color='black', size=11)))])
+    st.plotly_chart(fig)
+
+def render_linkedin_card(business_name):
+    linkedin_json = get_linkedin_info(business_name)
+    st.write(linkedin_json)
+    linkedin_card = """
+                
+                <body>
+                <div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); max-width: 300px; margin: auto;text-align: center;font-family: arial;">
+                <img src="https://softwareengineeringdaily.com/wp-content/uploads/2020/02/LinkedIn.jpg" alt="John" style="width:100%">
+                <h1>Linkedin</h1>
+                <p class="title" style="color: grey;font-size: 18px;">Industry: {}</p>
+                <p class="title" style="color: grey;font-size: 18px;">HQ: </p>
+                <p class="title" style="color: grey;font-size: 18px;">Company Type: </p>
+                <p class="title" style="color: grey;font-size: 18px;">Founded:</p>
+                <p class="title" style="color: grey;font-size: 18px;">Employees: </p>
+                
+                <p><button style ="border: none;outline: 0;display: inline-block;padding: 8px;color: white;background-color: #000;text-align: center;cursor: pointer;width: 100%;font-size: 18px;">Confidence Score: 68%</button></p>
+                </div>
+
+                </body>""".format("Sammy")
+    #st.markdown(linkedin_card, unsafe_allow_html=True)
+
+def render_google_data(business_code,business_name,GMAPS_API_KEY):
+    json_data = google_data(business_code,business_name,GMAPS_API_KEY)
+    st.write(json_data)
+
+def color_code(status):
+    if status=='Pending':
+        return 'rgb(255, 255, 102)'
+    elif status=='Issued':
+        return 'rgb(204, 255, 229)'
+    elif status=='Cancelled':
+        return 'rgb(255, 153, 153)'
+    elif status=='Gone Out of Business':
+        return 'rgb(204, 153, 255)'
+    elif status=='Inactive':
+        return 'rgb(224, 224, 224)'
+
+
+def load_business():
+    business_data = load_data()
+    # row_num = st.number_input('Choose the display size', min_value=5)
+    business_name = st.selectbox('Select The Business you want to explore',business_data['BusinessName'].unique())
+    business_df = business_data[business_data['BusinessName']==business_name]
+    business_count = business_df['BusinessName'].count()
+    business_code = business_df['PostalCode'].unique()
+    
+    issue_df = process_issue(business_data, business_name)
+    render_plot(issue_df)
+    # if len(business_code)==1:
+    #     st.write("Found 1 Matching Pincode")
+    render_google_data(business_code,business_name,GMAPS_API_KEY)
+    
+    #    st.write(response.json())
+    st.write("There are "+str(business_count)+" Matches for "+business_name)
+    
+    if st.checkbox("View Records"):
+        st.write(business_df)
+    if st.checkbox("View On Map"):
+        st.map(business_df)
+    st.subheader("Linkedin")
+    render_linkedin_card(business_name)
+    
+
+if __name__ == "__main__":
+    main()
---
+++ google.py
@@ -0,0 +1,53 @@
+import requests
+import pandas as pd
+
+def google_data(business_code,business_name,GMAPS_API_KEY):   
+    for b_code in business_code:
+        info_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={}&inputtype=textquery&fields=place_id,photos,user_ratings_total,business_status,formatted_address,name,opening_hours,rating&key=".format(business_name+" "+str(b_code))+GMAPS_API_KEY
+        response = requests.get(info_url)
+        candidates = response.json()['candidates']
+        print(candidates)
+        ls = ['name','status','address','rating','number','review_time','review text','author url','website']
+        for candidate in candidates:
+            business_dict = dict((el,"Not found") for el in ls)
+            business_dict['score'] = 0
+            if 'name' in candidate:
+                business_dict['name'] = candidate['name']
+                business_dict['score'] +=1
+            if 'business_status' in candidate:
+                business_dict['status'] = candidate['business_status']
+                business_dict['score']+=1
+            if 'formatted_address' in candidate:
+                business_dict['address'] = candidate['formatted_address']
+                business_dict['score']+=1
+            if 'rating' in candidate:
+                business_dict['rating'] = str(candidate['rating'])
+                business_dict['score']+=1
+
+            if 'place_id' in candidate: 
+                place_id = candidate['place_id']
+                place_url = "https://maps.googleapis.com/maps/api/place/details/json?place_id={}&fields=website,types,reviews,photos,icon,formatted_phone_number&key=".format(str(place_id))+GMAPS_API_KEY
+                place_response = requests.get(place_url).json()['result']
+
+                if 'formatted_phone_number' in place_response:
+                    business_dict['number'] = place_response['formatted_phone_number']
+                    business_dict['score']+=1
+
+                if 'website' in place_response:
+                    business_dict['website'] = place_response['website']
+                    business_dict['score']+=1
+
+                if 'reviews' in place_response:
+                    review_list=[]
+                    for review in place_response['reviews']:
+                        review_list.append(review['time'])
+                    index_id = review_list.index(max(review_list))
+                    business_dict['review time'] = place_response['reviews'][index_id]['relative_time_description']
+                    business_dict['score']+=1
+                    business_dict['review text'] = place_response['reviews'][index_id]['text']
+                    business_dict['score']+=1
+                    business_dict['author url'] = place_response['reviews'][index_id]['author_url']
+                    business_dict['score']+=1
+
+    
+    return business_dict
