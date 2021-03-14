9fa978075f4630d3cf670b2bfe54fc8739b0ae65
saptarshidg <saptarshiduttagupta@gmail.com>
2021-03-13 22:18:03 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 import requests
 import plotly.graph_objects as go
 # from Linkedin_scraping import get_linkedin_info
+from canada_registry import get_registry_data
 from places_api import google_data
 
 GMAPS_API_KEY="AIzaSyBja88VRg3fkfpYcseDWpQXuVuOGdLKEBI"
@@ -15,10 +16,10 @@ def main():
 
     if app_mode=="MainPage":
         load_mainpage()
-    
+
     elif app_mode=="Business Tracker":
         load_business()
-    
+
 
 def load_mainpage():
     st.header("Welcome To the Business Tracker")
@@ -37,7 +38,7 @@ def lon(x):
 
 @st.cache
 def load_data():
-    df = pd.read_csv('business-licences-hackathon.csv', sep=';')  
+    df = pd.read_csv('business-licences-hackathon.csv', sep=';')
     df['Geom'] = df['Geom'].apply(check_nan)
     df['lat'] = df['Geom'].apply(lat)
     df['lon'] = df['Geom'].apply(lon)
@@ -60,12 +61,28 @@ def render_plot(df_company):
     line_color=[df_company.color], fill_color=[df_company.color],
     align='center', font=dict(color='black', size=11)))])
     st.plotly_chart(fig)
+def render_canada_registry_card(business_name):
+    canada_registry_info = get_registry_data(business_name)
+    canada_registry_card = """
+
+                <body>
+                <div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); max-width: 300px; margin: auto;text-align: center;font-family: arial;">
+                <img src="https://beta.canadasbusinessregistries.ca/assets/img/canada-wordmark.png" alt="John" style="width:100%">
+                <h1>Canada Business Registry</h1>
+                <p class="title" style="color: grey;font-size: 18px;">Business Number: {}</p>
+                <p class="title" style="color: grey;font-size: 18px;">Registry ID: {}</p>
+                <p class="title" style="color: grey;font-size: 18px;">Registered Office Number: {}</p>
+                <p class="title" style="color: grey;font-size: 18px;">Status:{}</p>
+                <p class="title" style="color: grey;font-size: 18px;">Created: {}</p>
+                </div>
+                </body>""".format(canada_registry_info['Business Number'], canada_registry_info['Registry Id'], canada_registry_info['Registered Office Number'], canada_registry_info['Status'], canada_registry_info['Created'])
+    st.markdown(canada_registry_card, unsafe_allow_html=True)
 
 def render_linkedin_card(business_name):
     linkedin_json = get_linkedin_info(business_name)
     st.write(linkedin_json)
     linkedin_card = """
-                
+
                 <body>
                 <div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); max-width: 300px; margin: auto;text-align: center;font-family: arial;">
                 <img src="https://softwareengineeringdaily.com/wp-content/uploads/2020/02/LinkedIn.jpg" alt="John" style="width:100%">
@@ -75,7 +92,7 @@ def render_linkedin_card(business_name):
                 <p class="title" style="color: grey;font-size: 18px;">Company Type: </p>
                 <p class="title" style="color: grey;font-size: 18px;">Founded:</p>
                 <p class="title" style="color: grey;font-size: 18px;">Employees: </p>
-                
+
                 <p><button style ="border: none;outline: 0;display: inline-block;padding: 8px;color: white;background-color: #000;text-align: center;cursor: pointer;width: 100%;font-size: 18px;">Confidence Score: 68%</button></p>
                 </div>
 
@@ -86,7 +103,7 @@ def render_google_data(business_code,business_name,GMAPS_API_KEY):
     google_response = google_data(business_code,business_name,GMAPS_API_KEY)
     # st.write(json_data)
     google_card = """
-                
+
                 <body>
                 <div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); max-width: 300px; margin: auto;text-align: center;font-family: arial;">
                 <img src="" alt="John" style="width:100%">
@@ -100,14 +117,14 @@ def render_google_data(business_code,business_name,GMAPS_API_KEY):
                 <p class="title" style="color: grey;font-size: 18px;"><b>Review</b>:{}</p>
                 <p class="title" style="color: grey;font-size: 18px;"><b>Author URL</b>: {}</p>
                 <p class="title" style="color: grey;font-size: 18px;"><b>Website</b>:{}</p>
-                
+
                 <p><button style ="border: none;outline: 0;display: inline-block;padding: 8px;color: white;background-color: #000;text-align: center;cursor: pointer;width: 100%;font-size: 18px;">Confidence Score: 68%</button></p>
                 </div>
 
                 </body>""".format(google_response['name'], google_response['status'], google_response['address'], google_response['rating'], google_response['number'], google_response['review_time'], google_response['review text'], google_response['author url'], google_response['website'])
 
     st.markdown(google_card, unsafe_allow_html=True)
-    
+
 
 def color_code(status):
     if status=='Pending':
@@ -121,6 +138,19 @@ def color_code(status):
     elif status=='Inactive':
         return 'rgb(224, 224, 224)'
 
+def render_confidence_score(score):
+    progress_bar_html = """
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+                         <body>
+                         <div class="w3-container">
+                         <div class="w3-light-grey">
+                         <h4>Company Confidence Score</h4>
+                         <div class="w3-container w3-green w3-center" style="width:{}%">{}</div>
+                         </div><br>
+                         </div>
+                         """.format(score, score)
+    st.markdown(progress_bar_html, unsafe_allow_html=True)
 
 def load_business():
     business_data = load_data()
@@ -129,23 +159,25 @@ def load_business():
     business_df = business_data[business_data['BusinessName']==business_name]
     business_count = business_df['BusinessName'].count()
     business_code = business_df['PostalCode'].unique()
-    
+
     issue_df = process_issue(business_data, business_name)
     render_plot(issue_df)
     # if len(business_code)==1:
     #     st.write("Found 1 Matching Pincode")
     render_google_data(business_code,business_name,GMAPS_API_KEY)
-    
+
     #    st.write(response.json())
     st.write("There are "+str(business_count)+" Matches for "+business_name)
-    
+
     if st.checkbox("View Records"):
         st.write(business_df)
     if st.checkbox("View On Map"):
         st.map(business_df)
     st.subheader("Linkedin")
     # render_linkedin_card(business_name)
-    
+    render_canada_registry_card(business_name)
+    score=60
+    render_confidence_score(score)
 
 if __name__ == "__main__":
     main()
